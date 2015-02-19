da0673a86a80d11a5f034cb53a23659d9af51ed9
Jan Šebetovský <jan@sebetovsky.cz>
2015-02-19 14:37:23 +0100

    First version of czech numbers to words converter.

---
+++ src/Humanizer.Tests/Localisation/cs/NumberToWordsTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace Humanizer.Tests.Localisation.csCZ
+{
+    public class NumberToWordsTests : AmbientCulture
+    {
+        public NumberToWordsTests() : base("cs-CZ") { }
+
+        [Theory]
+        [InlineData(0, "nula")]
+        [InlineData(1, "jeden")] // TODO:Šeby:Rod: Zatím nemáme implementovaný číslovkový rod, ale správně by tady mělo být „jedna“.
+        [InlineData(2, "dva")]
+        [InlineData(3, "tři")]
+        [InlineData(4, "čtyři")]
+        [InlineData(5, "pět")]
+        [InlineData(6, "šest")]
+        [InlineData(7, "sedm")]
+        [InlineData(8, "osm")]
+        [InlineData(9, "devět")]
+        [InlineData(10, "deset")]
+        [InlineData(11, "jedenáct")]
+        [InlineData(12, "dvanáct")]
+        [InlineData(13, "třináct")]
+        [InlineData(14, "čtrnáct")]
+        [InlineData(15, "patnáct")]
+        [InlineData(16, "šestnáct")]
+        [InlineData(17, "sedmnáct")]
+        [InlineData(18, "osmnáct")]
+        [InlineData(19, "devatenáct")]
+        [InlineData(20, "dvacet")]
+        [InlineData(30, "třicet")]
+        [InlineData(40, "čtyřicet")]
+        [InlineData(50, "padesát")]
+        [InlineData(60, "šedesát")]
+        [InlineData(70, "sedmdesát")]
+        [InlineData(80, "osmdesát")]
+        [InlineData(90, "devadesát")]
+        [InlineData(100, "jedno sto")]
+        [InlineData(112, "jedno sto dvanáct")]
+        [InlineData(128, "jedno sto dvacet osm")]
+        [InlineData(1000, "jeden tisíc")]
+        [InlineData(2000, "dva tisíce")]
+        [InlineData(5000, "pět tisíc")]
+        [InlineData(10000, "deset tisíc")]
+        [InlineData(20000, "dvacet tisíc")]
+        [InlineData(21000, "dvacet jeden tisíc")]
+        [InlineData(22000, "dvacet dva tisíce")]
+        [InlineData(25000, "dvacet pět tisíc")]
+        [InlineData(100000, "jedno sto tisíc")]
+        [InlineData(500000, "pět set tisíc")]
+        [InlineData(1000000, "jeden milión")]
+        [InlineData(2000000, "dva milióny")]
+        [InlineData(5000000, "pět miliónů")]
+        [InlineData(1000000000, "jedna miliarda")]
+        [InlineData(2000000000, "dvě miliardy")]
+        [InlineData(1501001892, "jedna miliarda pět set jeden milión jeden tisíc osm set devadesát dva")]
+        [InlineData(2147483647, "dvě miliardy jedno sto čtyřicet sedm miliónů čtyři sta osmdesát tři tisíce šest set čtyřicet sedm")]
+        [InlineData(-1501001892, "mínus jedna miliarda pět set jeden milión jeden tisíc osm set devadesát dva")]
+        // Following tests are from the site of Institute For Czech Language of The Czech Academy Of Science (http://prirucka.ujc.cas.cz/?id=791&dotaz=%C4%8D%C3%ADsla%20slovy)
+        [InlineData(21, "dvacet jeden")] // TODO:Šeby:Rod: Zatím nemáme implementovaný číslovkový rod, ale správně by tady mělo být „jedna“.
+        [InlineData(54, "padesát čtyři")]
+        [InlineData(900, "devět set")]
+        [InlineData(2168, "dva tisíce jedno sto šedesát osm")]
+        [InlineData(2231, "dva tisíce dvě stě třicet jeden")] // TODO:Šeby:Rod: Zatím nemáme implementovaný číslovkový rod, ale správně by tady mělo být „jedna“.
+        [InlineData(1300768, "jeden milión tři sta tisíc sedm set šedesát osm")] // originally "jeden milion tři sta tisíc sedm set šedesát osm"
+        public void ToWords(int number, string expected)
+        {
+            Assert.Equal(expected, number.ToWords());
+        }
+
+        [Theory]
+        [InlineData(531, "pět set třicet jedna", GrammaticalGender.Feminine)]
+        [InlineData(531, "pět set třicet jeden", GrammaticalGender.Masculine)]
+        [InlineData(531, "pět set třicet jedno", GrammaticalGender.Neuter)]
+        [InlineData(122, "jedno sto dvacet dvě", GrammaticalGender.Feminine)]
+        [InlineData(122, "jedno sto dvacet dva", GrammaticalGender.Masculine)]
+        [InlineData(122, "jedno sto dvacet dvě", GrammaticalGender.Neuter)]
+        public void ToWordsWithGender(int number, string expected, GrammaticalGender gender)
+        {
+            Assert.Equal(expected, number.ToWords(gender));
+        }
+    }
+}
---
+++ src/Humanizer/Localisation/NumberToWords/CzechNumberToWordsConverter.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Humanizer.Localisation.NumberToWords
+{
+    internal class CzechNumberToWordsConverter : GenderedNumberToWordsConverter
+    {
+        private static readonly string[] HundredsMap = { "nula", "jedno sto", "dvě stě", "tři sta", "čtyři sta", "pět set", "šest set", "sedm set", "osm set", "devět set" };
+        private static readonly string[] TensMap = { "nula", "deset", "dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát" };
+        private static readonly string[] UnitsMap = { "nula", "jedna", "dvě", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět", "deset", "jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct" };
+
+        private readonly CultureInfo _culture;
+
+        public CzechNumberToWordsConverter(CultureInfo culture)
+        {
+            _culture = culture;
+        }
+
+        private static void CollectPartsUnderThousand(ICollection<string> parts, int number, GrammaticalGender gender)
+        {
+            var hundreds = number/100;
+            if (hundreds > 0)
+            {
+                parts.Add(HundredsMap[hundreds]);
+                number = number%100;
+            }
+
+            var tens = number/10;
+            if (tens > 1)
+            {
+                parts.Add(TensMap[tens]);
+                number = number%10;
+            }
+
+            if (number > 0)
+            {
+                if (number == 1 && gender == GrammaticalGender.Feminine)
+                    parts.Add("jedna");
+                else if (number == 1 && gender == GrammaticalGender.Masculine)
+                    parts.Add("jeden");
+                else if (number == 1 && gender == GrammaticalGender.Neuter)
+                    parts.Add("jedno");
+                else if (number == 2 && gender == GrammaticalGender.Feminine)
+                    parts.Add("dvě");
+                else if (number == 2 && gender == GrammaticalGender.Masculine)
+                    parts.Add("dva");
+                else if (number == 2 && gender == GrammaticalGender.Neuter)
+                    parts.Add("dvě");
+                else if (number < 20)
+                    parts.Add(UnitsMap[number]);
+            }
+        }
+
+        private static int GetMappingIndex(int number)
+        {
+            if (number == 1)
+                return 0;
+
+            if (number > 1 && number < 5)
+                return 1; //denominator
+
+            var tens = number / 10;
+            if (tens > 1)
+            {
+                var unity = number % 10;
+
+                if (unity == 1)
+                    return 0;
+
+                if (unity > 1 && unity < 5)
+                    return 1; //denominator
+            }
+
+            return 2; //genitive
+        }
+
+        public override string Convert(int number, GrammaticalGender gender)
+        {
+            if (number == 0)
+                return "nula";
+
+            var parts = new List<string>();
+
+            if (number < 0)
+            {
+                parts.Add("mínus");
+                number = -number;
+            }
+
+            // TODO:Šeby:Vyšší čísla: Dopsat vyšší čísla než je miliarda – nutné předělat na decimal
+
+            var milliard = number/1000000000;
+            if (milliard > 0)
+            {
+                // TODO:Šeby:Jeden: pro nezobrazení se nesmí rovnat jedničce.
+                if (milliard >= 1)
+                    CollectPartsUnderThousand(parts, milliard, GrammaticalGender.Feminine);
+
+                var map = new[] { "miliarda", "miliardy", "miliard" }; //one, denominator, genitive
+                parts.Add(map[GetMappingIndex(milliard)]);
+                number %= 1000000000;
+            }
+
+            var million = number/1000000;
+            if (million > 0)
+            {
+                // TODO:Šeby:Jeden: pro nezobrazení se nesmí rovnat jedničce.
+                if (million >= 1)
+                    CollectPartsUnderThousand(parts, million, GrammaticalGender.Masculine);
+
+                var map = new[] { "milión", "milióny", "miliónů" }; //one, denominator, genitive
+                parts.Add(map[GetMappingIndex(million)]);
+                number %= 1000000;
+            }
+
+            var thouthand = number/1000;
+            if (thouthand > 0)
+            {
+                // TODO:Šeby:Jeden: pro nezobrazení se nesmí rovnat jedničce.
+                if (thouthand >= 1)
+                    CollectPartsUnderThousand(parts, thouthand, GrammaticalGender.Masculine);
+
+                var thousand = new[] { "tisíc", "tisíce", "tisíc" }; //one, denominator, genitive
+                parts.Add(thousand[GetMappingIndex(thouthand)]);
+                number %= 1000;
+            }
+
+            var units = number/1;
+            if (units > 0)
+                CollectPartsUnderThousand(parts, units, gender);
+
+            return string.Join(" ", parts);
+        }
+
+        public override string ConvertToOrdinal(int number, GrammaticalGender gender)
+        {
+            return number.ToString(_culture);
+        }
+    }
+}
